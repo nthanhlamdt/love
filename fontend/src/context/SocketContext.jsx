1c6715381f5d59f4a226ead566eb09ea76b4c57d
Thành Lâm <126042321+nthanhlamdt@users.noreply.github.com>
2025-01-07 14:48:52 +0700

    Update SocketContext.jsx

--- fontend/src/context/SocketContext.jsx
+++ fontend/src/context/SocketContext.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react-refresh/only-export-components */
 import { createContext, useContext, useEffect, useState } from 'react'
-import { useAuthContext } from './AuthContext.jsx
+import { useAuthContext } from './authContext
   '
 import io from 'socket.io-client'
 
