5944a0be5f55a8218de14cdbe71c90baa36b764d
Thành Lâm <126042321+nthanhlamdt@users.noreply.github.com>
2025-01-07 15:36:32 +0700

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -40,7 +40,7 @@ app.use('/api/cook', cookRoutes)
 app.use('/api/message', messageRoutes)
 
 connectDB().then(() => {
-  server.listen(process.env.PORT, () => {
+  server.listen(process.env.PORT || 3000, () => {
     console.log(`Server is running on http://localhost:${process.env.PORT}`)
   })
 }).catch(error => {
