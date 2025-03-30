1801a86829f862424b7d954184fb34977cc33dfa
Thanh Lam <nthanhlamdt@gmail.com>
2025-03-30 20:22:23 +0700

    update 3

--- fontend/vite.config.js
+++ fontend/vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/',
 
   server: {
     host: '0.0.0.0', // Cho phép kết nối từ mọi địa chỉ IP trong mạng LAN
